942f4c226af198f9908293d4001a35c1092f353e
Sorokin Vladislav <sorokinvladislav1402@gmail.com>
2020-03-10 00:19:12 +0600

    add actions and mutations types

---
+++ src/store/actions.types.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
+export const FETCH_USER_PROFILE = 'FETCH_USER_PROFILE';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const SIGN_UP = 'SIGN_UP';
