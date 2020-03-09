2d08f580c5309b8f51c240c842fb32f404eea1b5
Sorokin Vladislav <sorokinvladislav1402@gmail.com>
2020-03-09 17:12:19 +0600

    add signup form

---
+++ src/views/Signup.vue
@@ -0,0 +1,59 @@
+<template>
+<form @submit.prevent>
+    <h1>Get Started</h1>
+
+    <label for="email2">Email</label>
+    <input v-model.trim="signupForm.email" type="text" placeholder="you@email.com" id="email2" />
+
+    <label for="password2">Password</label>
+    <input v-model.trim="signupForm.password" type="password"
+     placeholder="min 6 characters" id="password2" />
+
+    <label for="confirmPassword2">Confirm password</label>
+    <input v-model.trim="signupForm.password" type="password" id="confirmPassword2" />
+
+    <button @click="signup" class="button">Sign Up</button>
+
+    <div class="extras" @click="backToLogin">
+        <a>Back to Log In</a>
+    </div>
+</form>
+</template>
+
+<script>
+import { auth, usersCollection } from '../../firebaseConfig';
+
+export default {
+  name: 'login',
+  components: {},
+  data() {
+    return {
+      signupForm: {
+        email: '',
+        password: '',
+        confirmPass: '',
+      },
+    };
+  },
+  methods: {
+    backToLogin() {
+      this.$router.replace({ name: 'Login' });
+    },
+    async signup() {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        this.signupForm.email,
+        this.signupForm.password,
+      );
+
+      // create user obj
+      await usersCollection
+        .doc(user.uid)
+        .set({
+          email: this.signupForm.email,
+        });
+
+      this.$router.replace({ name: 'Home' });
+    },
+  },
+};
+</script>
