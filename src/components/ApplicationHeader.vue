1b3f8225364f568cc1d1e6a6b6429df139bf3161
Sorokin Vladislav <sorokinvladislav1402@gmail.com>
2020-03-09 17:30:46 +0600

    add sign out ability

---
+++ src/components/ApplicationHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <navigation />
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation.vue';
+
+export default {
+  name: 'application-header',
+  components: { Navigation },
+
+};
+</script>
